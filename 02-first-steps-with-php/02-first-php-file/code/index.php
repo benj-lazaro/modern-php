fb575bb94dc819dc64ec194e827417feabc0f58c
benj-lazaro <benjie.work@gmail.com>
2025-06-01 18:29:52 +0800

    02 the first php file & the echo command

---
+++ 02-first-steps-with-php/02-first-php-file/code/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1><?php echo 'Hello from PHP!'; ?></h1>
+
+    <h1>Hey, this is my first .php file</h1>
+
+    <!-- Generate a random number between integers 1 and 6 -->
+    <h1>Random number: <?php echo rand(1, 6); ?></h1>
+
+</body>
+
+</html>
