f3b060fc8ac9692e185140559c28edca56ef2f39
benj-lazaro <benjie.work@gmail.com>
2025-06-03 17:22:08 +0800

    02 strings within single quotes & . operator

---
+++ 03-data-types-&-logic/02-single-quote-strings/code/string.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="simple.css" />
+  <title>Document</title>
+</head>
+
+<body>
+  <pre>
+<?php
+// String concatenation using .
+$greeting = 'I\'m a string';
+echo $greeting . '!!! ' . $greeting . '?';
+
+echo '<br />';
+
+$name = 'John';
+$subject = 'PHP';
+$text = 'I\'m ' . $name . ' and currently learning the ' . $subject . ' language';
+echo $text . '.';
+?>
+  </pre>
+</body>
+
+</html>
