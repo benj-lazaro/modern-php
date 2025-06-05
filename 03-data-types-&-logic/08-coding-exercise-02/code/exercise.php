ee84f7fa1b64bff7ea50e27df401cfe1bb86fdf9
benj-lazaro <benjie.work@gmail.com>
2025-06-05 19:12:04 +0800

    08 coding exercise 02 tutortial 02

---
+++ 03-data-types-&-logic/08-coding-exercise-02/code/exercise.php
@@ -0,0 +1,6 @@
+<?php
+
+// Two variables $itemName and $itemPrice will be provided to you
+// Write your code here
+
+echo "Thank you for purchasing the {$itemName}! Your total is \${$itemPrice}.";
