1d66eb64876b7bc2863aacb7c9b98065a0c4fe85
benj-lazaro <benjie.work@gmail.com>
2025-06-15 15:34:03 +0800

    11 the foreach loop

---
+++ 04-arrays-and-for-each-loop/11-foreach/code/foreach.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="./simple.css" />
+    <title>Document</title>
+</head>
+
+<body>
+    <pre><?php
+
+            $categories = [
+                'Programming',
+                'Business',
+                'Art & Drawing',
+                'Self improvement',
+                'History',
+            ];
+
+            foreach ($categories as $category) {
+                var_dump($category);
+
+                if ($category === 'Business') {
+                    echo 'BUSINESS' . "\n";
+                }
+            }
+            ?></pre>
+
+    <!-- Alternative foreach syntax -->
+    <ul>
+        <?php foreach ($categories as $category): ?>
+            <li><?php echo $category; ?></li>
+        <?php endforeach; ?>
+    </ul>
+
+    <br />
+    <br />
+    <br />
+
+</body>
+
+</html>
