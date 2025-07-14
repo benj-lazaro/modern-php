71d561e4ae84f3f7cda83a2c0f63fffca9d18bce
benj-lazaro <benjie.work@gmail.com>
2025-07-14 23:27:21 +0800

    07 coding exercise 17 marketing campaign tracker 01 initial commit

---
+++ 09-nested-arrays/07-coding-exercise-17/code/exercise.php
@@ -0,0 +1,60 @@
+<?php
+
+// The $campaigns array will be provided to you
+$campaigns = [
+    'Spring Refresh' => [
+        'AdSet1' => [
+            'name' => 'Home Decor'
+        ]
+    ],
+    'Summer Blast' => [
+        'AdSet1' => [
+            'name' => 'Pool Accessories'
+        ],
+        'AdSet2' => [
+            'name' => 'BBQ Essentials'
+        ]
+    ],
+    'Fall Favorites' => [
+        'AdSet1' => [
+            'name' => 'Leaf Blowers'
+        ],
+        'AdSet2' => [
+            'name' => 'Halloween Costumes'
+        ],
+        'AdSet3' => [
+            'name' => 'Pumpkin Spice Everything'
+        ]
+    ],
+    'Winter Wonders' => [
+        'AdSet1' => [
+            'name' => 'Heated Blankets'
+        ]
+    ],
+    'Tech Trends' => [
+        'AdSet1' => [
+            'name' => 'Eco-friendly Gadgets'
+        ],
+        'AdSet2' => [
+            'name' => 'Smart Home Devices'
+        ]
+    ]
+];
+
+// Write our code here
+if (!empty($campaigns)):
+    foreach ($campaigns as $campaignName => $adSets):
+
+        foreach ($adSets as $adSet):
+            $adSetNames[] = '"' . $adSet['name'] . '"';
+        endforeach;
+
+        $formattedAdSetNames = implode(", ", $adSetNames);
+
+        $outputString = "- $campaignName: $formattedAdSetNames \n";
+        echo $outputString;
+
+        $adSetNames = [];
+
+    endforeach;
+endif;
