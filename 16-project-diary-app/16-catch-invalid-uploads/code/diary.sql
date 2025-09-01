2717708a1f53d51fda27294189babee7dc8391bb
benj-lazaro <benjie.work@gmail.com>
2025-09-01 17:03:59 +0800

    16 catch invalid uploads

---
+++ 16-project-diary-app/16-catch-invalid-uploads/code/diary.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Sep 01, 2025 at 10:38 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `diary`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `entries`
+--
+
+CREATE TABLE `entries` (
+  `id` int(11) NOT NULL,
+  `title` varchar(1023) NOT NULL,
+  `message` text NOT NULL,
+  `date` date NOT NULL,
+  `image` varchar(1023) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `entries`
+--
+
+INSERT INTO `entries` (`id`, `title`, `message`, `date`, `image`) VALUES
+(1, 'PHP is amazing!', 'PHP, a widely used server-side scripting language, stands out for its remarkable ease of use, extensive community support, and flexibility. It integrates seamlessly with HTML, making it ideal for web development, and offers a vast array of frameworks that streamline the development process. \r\nPHP\'s compatibility with various databases, its cost-effectiveness (being open-source), and its constant evolution with regular updates contribute to its enduring popularity and cool factor in the web development world.', '2045-01-05', NULL),
+(2, 'Test message', 'This is a test message...', '2050-01-05', NULL),
+(3, 'Another test message', 'This is another test message', '2060-03-02', NULL),
+(17, 'Return of the iconic Galaxy class starship', 'The unexpected return of the USS Enterprise-D (NCC-1701-D).', '2025-09-01', 'galaxyclass-1756715832.jpg'),
+(18, 'An entry with a non-image file attachment', 'An entry with a non-image file attachment...', '2025-09-01', NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `entries`
+--
+ALTER TABLE `entries`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `entries`
+--
+ALTER TABLE `entries`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
