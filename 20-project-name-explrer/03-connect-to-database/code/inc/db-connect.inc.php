31ea748ca5f7b93b191e013fa425694900a71ef0
benj-lazaro <benjie.work@gmail.com>
2025-09-20 15:47:48 +0800

    03 connect to database

---
+++ 20-project-name-explrer/03-connect-to-database/code/inc/db-connect.inc.php
@@ -0,0 +1,16 @@
+<?php
+
+try {
+    $pdo = new PDO('mysql:host=localhost;dbname=names;charset=utf8mb4', 'names', 'V5YWrb8*))pzOHF)', [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]);
+} catch (PDOException $e) {
+    // var_dump($e->getMessage());
+    echo 'A problem occured with the database connection...';
+    die();
+}
+
+// Test database connectivity
+// $stmt = $pdo->prepare('SELECT * FROM `names`');
+// $stmt->execute();
+// var_dump($stmt->fetch(PDO::FETCH_ASSOC)); // Output the 1st record
