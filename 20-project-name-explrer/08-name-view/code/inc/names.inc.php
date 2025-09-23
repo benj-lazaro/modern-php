d33eedc342a11f8eba20473eb9f7755bb93b0e1e
benj-lazaro <benjie.work@gmail.com>
2025-09-23 15:39:14 +0800

    08 solution develop the name view

--- 20-project-name-explrer/08-name-view/code/inc/names.inc.php
+++ 20-project-name-explrer/08-name-view/code/inc/names.inc.php
@@ -20,45 +20,12 @@ function fetch_names_by_initial(string $char): array
     return $names;
 }
 
-function fetch_name(string $name): array
+function fetch_name_entries(string $name): array
 {
     global $pdo;
 
-    $stmt = $pdo->prepare("SELECT * FROM `names` WHERE `name` = :expr ORDER BY `year` ASC");
-    $stmt->bindValue(':expr', "{$name}");
+    $stmt = $pdo->prepare("SELECT * FROM `names` WHERE `name` = :name ORDER BY `year` ASC");
+    $stmt->bindValue(':name', "{$name}");
     $stmt->execute();
-    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-    $nameData = [];
-
-    foreach ($results as $result):
-        $nameData[] = ["name" => $result['name'], "year" => $result['year'], "count" => $result['count']];
-    endforeach;
-
-    return $nameData;
-}
-
-function display_name(array $nameData): void
-{
-    if (!empty($nameData)):
-        echo "<table>";
-        echo "<thead><tr><th>Name</th><th>Year</th><th>Count</th></tr></thead>";
-        echo "<tbody>";
-        echo "</tbody>";
-
-        foreach ($nameData as $name):
-            echo "<tr>";
-            echo "<td>";
-            echo $name['name'];
-            echo "</td>";
-            echo "<td>";
-            echo $name['year'];
-            echo "</td>";
-            echo "<td>";
-            echo $name['count'];
-            echo "</td>";
-            echo "</tr>";
-        endforeach;
-        echo "</table>";
-    endif;
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
--- 20-project-name-explrer/08-name-view/code/name.php
+++ 20-project-name-explrer/08-name-view/code/name.php
@@ -3,12 +3,37 @@ require __DIR__ . '/inc/all.inc.php';
 
 $name = (string) ($_GET['name'] ?? '');
 
-$nameData = fetch_name($name);
+if (empty($name)):
+    header("Location: index.php");
+    die();
+endif;
+
+$entries = fetch_name_entries($name);
 
 ?>
-
 <?php require __DIR__ . '/views/header.php'; ?>
 
-<?php display_name($nameData); ?>
+<h1>Statistics for the name: <?php echo e($name); ?></h1>
+
+<?php if (empty($entries)): ?>
+    <p>Sorry, we could not find that name in our system. </p>
+<?php else: ?>
+    <table>
+        <thead>
+            <tr>
+                <th>Year</th>
+                <th>Number of babies born</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($entries as $entry): ?>
+                <tr>
+                    <td><?php echo e($entry['year']); ?></td>
+                    <td><?php echo e($entry['count']); ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+<?php endif; ?>
 
 <?php require __DIR__ . '/views/footer.php'; ?>
