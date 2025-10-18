0e62a17205f2628441b53252ed529ffa72d4d992
benj-lazaro <benjie.work@gmail.com>
2025-10-18 23:25:43 +0800

    02 create model class

---
+++ 23-project-city-explorer/02-create-model-class/code/views/pages/index.view.php
@@ -0,0 +1,11 @@
+<h1>List of cities:</h1>
+
+
+<ul>
+    <?php foreach ($entries as $city): ?>
+        <li>
+            <?php echo e($city->city); ?> (<?php echo e($city->country); ?>)
+        </li>
+    <?php endforeach; ?>
+
+</ul>
