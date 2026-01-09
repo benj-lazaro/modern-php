e14363bd3f9611fbf36c8cc74396009d634aa50e
benj-lazaro <benjie.work@gmail.com>
2026-01-09 18:41:40 +0800

    10 enhance code modularity utilize return values in file inclusions code refactor

--- 29-container-pattern/10-return-values/code/other-file.php
+++ 29-container-pattern/10-return-values/code/other-file.php
@@ -6,5 +6,5 @@ header("Content-Type: text/plain");
 echo "Hello! I'm a content w/in an external file.\n";
 
 // Simulates a function returning a stand-in string value as a PDO object
-$str = "I'm a simulated a PDO object return value    from a function w/in an external PHP file";
+$str = "I'm a simulated a PDO object return value from a function w/in an external PHP file";
 return $str;
