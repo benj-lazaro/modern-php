35c73d2d4920e60f00f51515bc0d4ff3efe6c924
benj-lazaro <benjie.work@gmail.com>
2026-01-16 19:14:57 +0800

    01 overview content & code refactor

--- 29-container-pattern/01-overview/code/simple-container.php
+++ 29-container-pattern/01-overview/code/simple-container.php
@@ -2,42 +2,31 @@
 
 header("Content-Type: text/plain");
 
-// Class(es) with dependencies
 class PostsRepository {
-    // Constructor
     public function __construct(private string $a, private string $b) {}
 }
 
 class PostsController {
-    // Constructor
     public function __construct(private PostsRepository $postsRepository) {}
 }
 
-// Every instance of PostsRepository requires passing dependencies (i.e. arguments)
-// Every instance of PostsController requires a PostsRepository object as argument
-// These steps are repeated throughout the codebase whenever a instance of PostController is required
-// Changes on either Classes requires implementing changes throughout the codebase
-
-// $postsRepository = new PostsRepository("A", "B");
-// $postsController = new PostsController($postsRepository);
-
 // Container pattern
 class Container {
-    // Methods
     public function getPostsRepository(): PostsRepository {
         return new PostsRepository("C", "D");
     }
 
     public function getPostsController(): PostsController {
+        // Requires an instance of Class PostsRepository
         $postsRepository = $this->getPostsRepository();
         return new PostsController($postsRepository);
     }
 }
 
-// Container is created ONE TIME ONLY
+
+// Container pattern instantiated ONLY ONCE
 $container = new Container();
 
-// Every since of PostsController ONLY requires this line of code
+// Instantiate a PostsController Object
 $postsController = $container->getPostsController();
-
 var_dump($postsController);
