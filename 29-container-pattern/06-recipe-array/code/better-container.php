e7c85b1aee28097e70cb999199d0f26c15946e42
benj-lazaro <benjie.work@gmail.com>
2026-01-18 00:50:44 +0800

    06 implement a recipe array for dynamic instance creation content & code refactor

--- 29-container-pattern/06-recipe-array/code/better-container.php
+++ 29-container-pattern/06-recipe-array/code/better-container.php
@@ -2,63 +2,70 @@
 
 header("Content-Type: text/plain");
 
-// Class(es) with dependencies
 class PostsRepository {
-    // Constructor
     public function __construct(private string $a, private string $b) {
-        var_dump("PostsRepository has been constructed...");
+        var_dump("PostsRespository instantiated...");
     }
 }
 
 class PostsController {
-    // Constructor
-    public function __construct(private PostsRepository $postsRepository) {}
+    public function __construct(private PostsRepository $postsRepository) {
+        var_dump("PostsController instantiated...");
+    }
 }
 
 // Container pattern
 class Container {
-    // Properties
     private array $instances = [];
     private array $recipes = [];
 
-    // Constructor
     public function __construct()
     {
-        $this->recipes['postsRepository'] = function() {
+        // Assign recipes into the array
+        $this->recipes['postsRepository'] = function () {
             return new PostsRepository("A", "B");
         };
 
-        $this->recipes['postsController'] = function() {
-            $postsRepository = $this->get("postsRepository");
+        $this->recipes['postsController'] = function () {
+            $postsRepository = $this->get('postsRepository');
             return new PostsController($postsRepository);
         };
     }
-  
-    // Methods
+
+    // Unified instance creation
     public function get($what) {
+        // If there is NO active instance of requested Class
         if (empty($this->instances[$what])):
+
+            // Check for the existence of the requested Class' recipe
             if (empty($this->recipes[$what])):
-                echo "Could not build: {$what}";
+                echo "Could NOT build: {$what}.\n";
                 die();
             endif;
-            
+
+            // Access recipe & save the new instance of the requested Class
             $this->instances[$what] = $this->recipes[$what]();
         endif;
 
+        // Returns either the new or current (active) instance
         return $this->instances[$what];
     }
 }
 
-// Create an instance of a Container; this is done ONLY ONCE
+// Container pattern instantiated ONLY ONCE
 $container = new Container();
 
-$postsRepository = $container->get("postsRepository");
+// Create an instance of PostsRepository
+$postsRepository = $container->get('postsRepository');
 var_dump($postsRepository);
 
 // Create an instance of PostController
-$postsController = $container->get("postsController");
+$postsController = $container->get('postsController');
 var_dump($postsController);
 
-// Intentional non-existent Controller
-$postsController2 = $container->get("postsController123");
+// Create another instance of PostController; uses the same current (active) instance
+$postsController2 = $container->get('postsController');
 var_dump($postsController2);
+
+// Deliberate non-existent Class
+$postsController3 = $container->get('postsController123');
